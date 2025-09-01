56b2e7e50d518bd5b38311b5f24a9b3551ef4e76
ABU SAID <abusaid@MacBook-Air-2.local>
2025-09-01 20:06:12 +0600

    Add FoodiFy-Server on the GitHub and implement post user get user delete user patch for give Admin role a Specific user as a Admin and get menu get reviews post carts get carts delete specific cart

---
+++ index.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+// const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const port = process.env.PORT || 5001;
+
+
+// middle wire
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.szzkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const userCollection = client.db("FoodiFyDB").collection("users");
+        const menuCollection = client.db("FoodiFyDB").collection("menu");
+        const reviewCollection = client.db("FoodiFyDB").collection("reviews");
+        const cartCollection = client.db("FoodiFyDB").collection("carts");
+
+        // user related api data
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // inserted email if user does not exists
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User Already Exist', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        // get user
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+        // delet users
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // provide admin reole 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        // JWT Related api's
+        // app.post('/jwt', async (req, res) => {
+        //     const user = req.body;
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY, {
+        //         expiresIn: '1h'
+        //     });
+        //     res.send({ token });
+        // })
+
+
+
+
+
+
+        // Menu related apis
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
+        // Reviews Data load 
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        // Card Colection 
+
+        // Post Cart into server side
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
+
+        })
+
+        // get card collection data
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
+        // Delete cart item from cart dashboard
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Foodify restaurant server is running');
+})
+
+app.listen(port, () => {
+    console.log(`FoodiFy Restaurent is running on ${port}`)
+})
